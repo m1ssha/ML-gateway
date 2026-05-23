f2cb46cd621ff7c5f0a7c7e79783402a4d0774ba
Mikhail Nechaev <m1ssha@vk.com>
2026-05-23 17:45:54 +0300

    исправление 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,9 +10,10 @@ export default defineConfig({
     }
   },
   server: {
+    host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8001',
+        target: 'http://backend:8001',
         changeOrigin: true,
       }
     }
