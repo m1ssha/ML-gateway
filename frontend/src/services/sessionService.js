5c9e7ae9e316918fd137cf75cea9e69608574b08
Mikhail Nechaev <m1ssha@vk.com>
2026-05-23 18:04:30 +0300

    исправление 10

--- frontend/src/services/sessionService.js
+++ frontend/src/services/sessionService.js
@@ -2,7 +2,7 @@ import api from './api'
 
 export default {
   async createSession() {
-    const response = await api.post('/sessions')
+    const response = await api.post('/sessions', {})
     return response.data
   },
   
