f0c92a5039f722e22de3a06019f0e56dab02ea9b
Mikhail Nechaev <m1ssha@vk.com>
2026-05-23 17:55:15 +0300

    исправление 5

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -11,6 +11,10 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+import os
+from app.config import settings
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
